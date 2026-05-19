2c9f07044a9b65ce89c1648cfb29a04bab2eadd0
Sweden <sweden.agunenye1@gmail.com>
2026-05-19 12:49:48 -0700

    inference

--- src/train.py
+++ src/train.py
@@ -6,17 +6,11 @@ import cv2
 model = YOLO("yolo11n.pt")
 
 # Train the model on COCO8 example (should be switched to produce images later)
-# coco8 = None
-# try:
-#     with open("/data/coco8.yaml", "r") as file:
-#         coco8 = yaml.safe_load(file)
-# except Exception as e:
-#     print(f"An unexpected error occurred: {e}")
 
 results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
 
-# Run inference
-img = cv2.imread("/Users/swedenagu/cs131_project/data/freshApple(1).jpg")
-cv2.imshow("image", img)
+# Run inference (image output testing first)
+# img = cv2.imread("/Users/swedenagu/cs131_project/data/freshApple(1).jpg")
+# cv2.imshow("image", img)
 
-# results = model
+results = model("data/freshApple(1).jpg")
